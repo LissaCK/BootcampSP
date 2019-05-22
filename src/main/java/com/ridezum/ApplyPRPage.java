b6dd64ff99d65a74935f78f3d04ea719262959f2
LissaCK <lesya.metelya@gmail.com>
2019-05-21 22:58:47 -0400

    Git commit

---
+++ src/main/java/com/ridezum/ApplyPRPage.java
@@ -0,0 +1,9 @@
+package com.ridezum;
+
+import org.openqa.selenium.WebDriver;
+
+public class ApplyPRPage extends ViewJobsPage {
+    public ApplyPRPage (WebDriver driver){
+        super(driver);
+    }
+}
